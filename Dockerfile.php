2ac7659b329f4226c8b4f25caf8236c5cd43866d
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-12-21 16:13:12 +0000

    restructure

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,12 +1,12 @@
 FROM php:8.2-fpm-alpine
-COPY . /var/www/html/
-
+COPY ./src /var/www/html/
+COPY composer_install.sh composer.json /var/www/html/
 RUN apk add --no-cache git libzip-dev zip \
     && docker-php-ext-install zip \
     && cd /var/www/html \
     && chmod +x composer_install.sh && ./composer_install.sh \ 
     && mv composer.phar /usr/local/bin/composer \
     && composer install --no-cache \
-    && rm composer_install.sh Dockerfile.caddy Dockerfile.php \
+    && rm composer_install.sh \
     && wget https://github.com/highlightjs/cdn-release/archive/refs/tags/11.9.0.zip \
     && unzip 11.9.0.zip && mv cdn-release-11.9.0/build /var/www/html/highlightjs && rm 11.9.0.zip
