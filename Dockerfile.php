7e31a6f709a8984d965d8630f1a999938dc51c74
Florian Ritterhoff <florian@ritterhoff.org>
2023-12-21 10:35:27 +0100

    fix highlightjs

--- Dockerfile.php
+++ Dockerfile.php
@@ -7,4 +7,6 @@ RUN apk add --no-cache git libzip-dev zip \
     && chmod +x composer_install.sh && ./composer_install.sh \ 
     && mv composer.phar /usr/local/bin/composer \
     && composer install --no-cache \
-    && rm composer_install.sh Dockerfile.caddy Dockerfile.php 
+    && rm composer_install.sh Dockerfile.caddy Dockerfile.php \
+    && wget https://github.com/highlightjs/cdn-release/archive/refs/tags/11.9.0.zip && \
+    && unzip 11.9.0.zip && mv cdn-release-11.9.0/build /var/www/html/highlightjs && rm 11.9.0.zip
