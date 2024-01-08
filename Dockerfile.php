c41185110286ecad44c31dcf650dcbc4d48a18e3
Mehmet Sagir <mehmet.sagir@hm.edu>
2024-01-08 16:13:05 +0000

    add instruction
    npm, webpack, 2nd Buildstep

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,5 @@
-FROM php:8.2-fpm-alpine
-COPY ./src /var/www/html/
+FROM php:8.2-fpm-alpine AS BUILD
+COPY . /var/www/html/
 COPY composer_install.sh composer.json /var/www/html/
 RUN apk add --no-cache git libzip-dev zip \
     && docker-php-ext-install zip \
@@ -9,4 +9,16 @@ RUN apk add --no-cache git libzip-dev zip \
     && composer install --no-cache \
     && rm composer_install.sh \
     && wget https://github.com/highlightjs/cdn-release/archive/refs/tags/11.9.0.zip \
-    && unzip 11.9.0.zip && mv cdn-release-11.9.0/build /var/www/html/highlightjs && rm 11.9.0.zip
+    && unzip 11.9.0.zip && mv cdn-release-11.9.0/build /var/www/html/src/highlightjs && rm 11.9.0.zip
+
+RUN apk add --no-cache npm nodejs \
+    #&& npm install --save-dev webpack webpack-cli webpack-merge html-webpack-plugin \
+    #&& npm install js-tiktoken \
+    && npm install \
+    && npx webpack --config config.production.js \
+    && mv -f dist/interface.js src/interface.js \
+    && mv vendor src
+
+FROM php:8.2-fpm-alpine
+WORKDIR /var/www/html
+COPY --from=BUILD /var/www/html/src /var/www/html
