b7e9800a1c82aa527bb8b385b249f40e60bff601
Mehmet Sagir <mehmet.sagir@hm.edu>
2024-01-10 14:30:23 +0000

    remove unused oidc scope

--- src/oidc_login.php
+++ src/oidc_login.php
@@ -23,15 +23,8 @@ if ($testuser) {
     $oidc->setHttpUpgradeInsecureRequests(false);
 }
 
-$oidc->addScope('profile','email');
+$oidc->addScope('email');
 $oidc->authenticate();
-
-// Set session variable username
-$firstname = $oidc->requestUserInfo('given_name');
-$surname = $oidc->requestUserInfo('family_name');
-$initials = substr($firstname, 0, 1) . substr($surname, 0, 1);
-#
-$_SESSION['initials'] = $initials;
 
 $_SESSION['username'] = $oidc->requestUserInfo('email');
 
