b7e84421f1dc2c3841dc3dba6fe0d7d10d7c108b
Mehmet S <sagir@hm.edu>
2024-06-13 12:36:12 +0000

    chore: install dependencies for sql

--- Dockerfile.php
+++ Dockerfile.php
@@ -9,8 +9,13 @@ RUN apk add --no-cache git libzip-dev zip \
     && composer install --no-cache \
     && rm composer_install.sh 
 
-
-
 FROM php:8.2-fpm-alpine
 WORKDIR /var/www/html
 COPY --from=BUILD /var/www/html /var/www/html
+ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/download/2.2.16/install-php-extensions /usr/local/bin/
+RUN install-php-extensions pdo pdo_pgsql
+
+
+#RUN apk update && apk add --no-cahche \
+#    libpq-dev \
+#    && docker-php-ext-install pdo pdo_pgsql
