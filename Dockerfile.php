0ba215464d38f60260c7a3f640a673b477c391e0
Mehmet S <sagir@hm.edu>
2024-07-18 10:43:55 +0000

    chore: update image, stage2 copy dependencies

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,21 +1,20 @@
-FROM php:8.2-fpm-alpine AS BUILD
+FROM php:8.3-fpm-alpine AS BUILD
 COPY . /var/www/html/
-#COPY composer_install.sh composer.json /var/www/html/
+ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/download/2.2.16/install-php-extensions /usr/local/bin/
 RUN apk add --no-cache git libzip-dev zip \
     && docker-php-ext-install zip \
     && cd /var/www/html \
     && chmod +x composer_install.sh && ./composer_install.sh \ 
     && mv composer.phar /usr/local/bin/composer \
     && composer install --no-cache \
-    && rm composer_install.sh 
+    && install-php-extensions pdo pdo_pgsql 
 
-FROM php:8.2-fpm-alpine
+FROM php:8.3-fpm-alpine
 WORKDIR /var/www/html
 COPY --from=BUILD /var/www/html /var/www/html
-ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/download/2.2.16/install-php-extensions /usr/local/bin/
-RUN install-php-extensions pdo pdo_pgsql
-
-
-#RUN apk update && apk add --no-cahche \
-#    libpq-dev \
-#    && docker-php-ext-install pdo pdo_pgsql
+# pdo pdo_pgsql dependencies
+COPY --from=BUILD /usr/lib/libpgtypes.so.* /usr/lib/
+COPY --from=BUILD /usr/lib/libpq.so.* /usr/lib/
+COPY --from=BUILD /usr/lib/libecpg.so.* /usr/lib
+COPY --from=BUILD /usr/local/etc/php/conf.d/docker-php-ext-pdo_pgsql.ini /usr/local/etc/php/conf.d/
+COPY --from=BUILD /usr/local/lib/php/extensions /usr/local/lib/php/extensions
