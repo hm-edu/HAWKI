87fd0d9b78c83e880a0d9fd620c0f2975c73a362
Mehmet S <sagir@hm.edu>
2024-09-17 12:48:17 +0000

    chore: update token_limiter

--- private/app/php/stream-api.php
+++ private/app/php/stream-api.php
@@ -39,8 +39,9 @@ $requestPayload = file_get_contents('php://input');
 $requestPayload = json_decode($requestPayload, true);
 $requestPayload['stream_options']['include_usage'] = true;
 $requestPayload = json_encode($requestPayload);
-//if environment variable = true
-check_token_limit();
+if ((isset($env) ? strtolower($env['TOKEN_DB']) : strtolower(getenv('TOKEN_DB'))) == "true") {
+	check_token_limit();
+}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $apiUrl);
@@ -54,7 +55,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 	'Content-Type: application/json'
 ]);
 curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $data) {
-	get_tokens($data);
+	if (strtolower(isset($env) ? $env['TOKEN_DB'] : getenv('TOKEN_DB')) == "true") {
+		get_tokens($data);
+	}
+	
 	echo $data;
 	if (ob_get_level() > 0) {
 		ob_flush();
@@ -73,51 +77,58 @@ curl_close($ch);
 
 
 function get_tokens($data){
-	$pattern = '/"usage":\{"prompt_tokens":[0-9]+,"completion_tokens":[0-9]+,"total_tokens":[0-9]+\}/';
-	if (preg_match_all($pattern, $data, $matches)) {
-		$last_match = end($matches[0]);
-		$numberpattern = '/[0-9]+/';
-		if (preg_match_all($numberpattern, $last_match, $numbers)){
-	
-			$prompt_tokens = $numbers[0][0];
-			$completion_tokens = $numbers[0][1];
-			$total_tokens = $numbers[0][2];
-			
-			$host = getenv("DB_HOST");
-			$db = getenv("DB_DB");
-			$table = getenv('DB_TABLE');
-			$user = getenv("DB_USER");
-			$pass = getenv("DB_PASS");
-			$port = getenv("DB_PORT");
-			$dsn = "pgsql:host=$host;port=$port;dbname=$db";
+	$jsonstrings= explode("data: ", $data);
+	foreach ($jsonstrings as &$jsonstring) {
+		$json = json_decode($jsonstring, true);
+		if (!isset($json) || $json['usage'] == null) {
+			continue;
+		}
+
+		$prompt_tokens = $json['usage']['prompt_tokens'];
+		$completion_tokens = $json['usage']['completion_tokens'];
+		$total_tokens = $json['usage']['total_tokens'];
+
+		$host = getenv("DB_HOST");
+		$db = getenv("DB_DB");
+		$table = getenv('DB_TABLE');
+		$user = getenv("DB_USER");
+		$pass = getenv("DB_PASS");
+		$port = getenv("DB_PORT");
+		$dsn = "pgsql:host=$host;port=$port;dbname=$db";
 			try{
 				$pdo = new PDO($dsn, $user, $pass);
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$sql = "INSERT INTO $table (username,datum,prompt_tokens,completion_tokens,total_tokens) VALUES(:username,:datum,:prompt,:completion,:total) ON CONFLICT (username,datum) DO UPDATE SET prompt_tokens = $table.prompt_tokens + EXCLUDED.prompt_tokens, completion_tokens = $table.completion_tokens + EXCLUDED.completion_tokens, total_tokens = $table.total_tokens + EXCLUDED.total_tokens;";
+				$sql = "INSERT INTO $table (username,datum,prompt_tokens,completion_tokens,total_tokens,model) VALUES(:username,:datum,:prompt,:completion,:total,:model) ON CONFLICT (username,datum,model) DO UPDATE SET prompt_tokens = $table.prompt_tokens + EXCLUDED.prompt_tokens, completion_tokens = $table.completion_tokens + EXCLUDED.completion_tokens, total_tokens = $table.total_tokens + EXCLUDED.total_tokens, model = $table.model;";
 				$stmt=$pdo->prepare($sql);
-
 				$stmt->bindParam(':username',$username);
 				$stmt->bindParam(':datum',$datum);
 				$stmt->bindParam(':prompt',$prompt);
 				$stmt->bindParam(':completion',$completion);
 				$stmt->bindParam(':total',$total);
+				$stmt->bindParam(':model',$model);
 
 				$username = $_SESSION['username'];
 				$datum = date("Y-m-d");
 				$prompt = $prompt_tokens;
 				$completion = $completion_tokens;
 				$total = $total_tokens;
+				global $requestPayload;
+				$model = json_decode($requestPayload, true)['model'];
 				$stmt->execute();
 
 			} catch (PDOException $e) {
 				error_log($e->getMessage(),0);
 			}
-		}
-		
-	}
+	}	
+	unset($jsonstring);
 }
 
 function check_token_limit(){
+	$max_tokens = getenv("TOKEN_LIMIT");
+	if ($max_tokens <= 0) {
+		return;
+	}
+
 	$host = getenv("DB_HOST");
 	$db = getenv("DB_DB");
 	$table = getenv('DB_TABLE');
@@ -125,13 +136,11 @@ function check_token_limit(){
 	$pass = getenv("DB_PASS");
 	$port = getenv("DB_PORT");
 	$dsn = "pgsql:host=$host;port=$port;dbname=$db";
-	$max_tokens = //environment variable
-
+	
 	try {
- 	   $pdo = new PDO($dsn, $user, $pass);
- 	   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// TODO: change query; last 3days
-	    $sql = "SELECT total_tokens FROM $table WHERE username = :username AND datum = :datum";
+ 		$pdo = new PDO($dsn, $user, $pass);
+ 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT SUM(total_tokens) AS total_tokens FROM $table WHERE datum >= (:datum ::date - INTERVAL '2 day')::date AND username = :username GROUP BY username;";
 	    $stmt = $pdo->prepare($sql);
 	    $stmt->bindParam(':username', $username);
 		$stmt->bindParam(':datum', $datum);
@@ -141,13 +150,24 @@ function check_token_limit(){
 	    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
 	    if ($result && $result['total_tokens'] >= $max_tokens) {
-	        error_log("Fehlermeldung: Die Gesamtzahl der Tokens überschreitet.");
+	        $message = $_SESSION['translation']['tokenUsedMessage'];
+			$response = [
+                'error' => true,
+                'choices' => [['delta' => ['content' => $message]]]
+            ];
+
+            echo "data: " . json_encode($response) . "\n\n";
+
+			if (ob_get_level() > 0) {
+                ob_flush();
+            }
+            flush();
+            
 	        exit;
 	    }
 	} catch (PDOException $e) {
 	    error_log($e->getMessage());
 	    exit;
 	}
-
 
 }
