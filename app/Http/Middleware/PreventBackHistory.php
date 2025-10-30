c639927388a42e77a491e84eff62fecc605cfe8b
Mehmet S <sagir@hm.edu>
2025-10-30 10:35:17 +0000

    feat: add Token Limit

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -21,10 +21,19 @@ class PreventBackHistory
              return $next($request);
          }
          $response = $next($request);
+         //if (method_exists($response, 'headers')) {
+            $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
+            $response->headers->set('Pragma','no-cache');
+            $response->headers->set('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+       // } else {
+ 
+       //  $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+        //        ->header('Pragma','no-cache')
+       //         ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+      //  }
 
-         return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
-             ->header('Pragma','no-cache')
-             ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
-
+        return $response;
+        
+ 
      }
 }
