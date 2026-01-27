196eb4120bba73ecfa5bd66ba837bb10125203a6
Mehmet S <sagir@hm.edu>
2026-01-27 11:02:12 +0000

    include link to dataprotection

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -42,7 +42,7 @@
                 <x-icon name="settings-icon"/>
             </button>
             <div class="impressumPanel">
-                <a href="/dataprotection" target="_blank">{{ $translation["DataProtection"] }}</a>
+                <a href="{{ env("DATAPROTECTION_LOCATION") }}" target="_blank">{{ $translation["DataProtection"] }}</a>
                 <a href="{{ env("IMPRINT_LOCATION") }}" target="_blank">{{ $translation["Impressum"] }}</a>
             </div>
         </div>
